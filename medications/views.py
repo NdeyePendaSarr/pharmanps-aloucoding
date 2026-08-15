392c64cc9424ebfb20101d89253880333fc26b23
Ndeye Penda SARR <ndasarr884@gmail.com>
2026-08-15 22:35:49 +0000

    Fix: import manquant de ProtectedError (crash 500 a la suppression d'un medicament vendu)

--- medications/views.py
+++ medications/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, ProtectedError
 from .models import Medication, Category, StockMovement
 from django.db.models import Sum # Non utilisé ici mais bonne pratique de l'avoir si besoin d'agrégation
 
